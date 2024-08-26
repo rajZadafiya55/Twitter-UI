9f4483dc9dff86721274f205d252e13824c4c55b
rajZadafiya55 <zadagfiyaraj395.com>
2024-08-26 13:23:51 +0530

    fc

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, 
+    port: 3000,
     proxy: {
       "/api": {
         target: "https://twitter-api-2viw.onrender.com",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // This will remove "/api" from the forwarded request
       },
     },
   },
